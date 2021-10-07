8b1eae8bc99d24599d6f8aaf23b1d0e8bd902a82
Jesper Hermann Munk <jesper@speedware.dk>
2021-10-07 15:10:42 +0200

    Add project files.

---
+++ ShareButtonIssue/ShareButtonIssue/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ShareButtonIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            EnableShareAction();
+        }
+
+
+        private void EnableShareAction()
+        {
+            var downloadToolbarItem = new ToolbarItem();
+            downloadToolbarItem.IconImageSource = "share_apple_white";
+            downloadToolbarItem.Text = "Share";
+            downloadToolbarItem.Clicked += async (object sender, EventArgs e) =>
+            {
+                await ShareFile();
+            };
+
+            ToolbarItems.Add(downloadToolbarItem);
+        }
+
+        private static async Task ShareFile()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = Path.Combine(path, "TestPDF.pdf");
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = "Pdf file",
+                File = new ShareFile(file)
+            });
+        }
+    }
+}
